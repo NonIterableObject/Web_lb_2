b453a9bc472cc5e11831ffd100ba42c0ab3a1b1e
Decamiron <vlad.krutik2002@gmail.com>
2023-03-19 17:13:28 +0200

    init

---
+++ script.js
@@ -0,0 +1,43 @@
+// Get references to HTML elements
+const dataTable = document.getElementById("dataTable");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const intervalInput = document.getElementById("interval");
+
+// Initialize variables
+let dataRows = [];
+let timerId = null;
+let rowCount = 0;
+
+// Generate a new row of data and add it to the table
+const addDataRow = () => {
+    const row = dataTable.insertRow(1);
+    row.classList.add(rowCount % 2 === 0 ? "even" : "odd");
+    const randomNumber = Math.floor(Math.random() * 1000);
+    const dateTime = new Date().toLocaleString();
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    cell1.innerText = randomNumber;
+    cell2.innerText = dateTime;
+    rowCount++;
+    dataRows.push(row);
+};
+
+// Start generating new rows of data at the specified interval
+const startGenerating = () => {
+    const interval = parseInt(intervalInput.value) * 1000;
+    timerId = setInterval(addDataRow, interval);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+};
+
+// Stop generating new rows of data
+const stopGenerating = () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+};
+
+// Attach event listeners
+startBtn.addEventListener("click", startGenerating);
+stopBtn.addEventListener("click", stopGenerating);
