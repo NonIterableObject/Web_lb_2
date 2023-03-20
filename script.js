289fbadbd542b760852bdbf3a4637ddb477125b2
Decamiron <vlad.krutik2002@gmail.com>
2023-03-20 13:50:45 +0200

    fix typo

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ let rowCount = 0; //For coloring even and odd columns of a table
 
 // Generate a new row of data and add it to the table
 const addDataRow = () => {
-    const row = dataTable.insertRow(1);
+    let row = dataTable.insertRow(1);
     row.classList.add(rowCount % 2 === 0 ? "even" : "odd");
-    const randomNumber = Math.floor(Math.random() * 1000);
-    const dateTime = new Date().toLocaleString();
-    const cell1 = row.insertCell(0);
-    const cell2 = row.insertCell(1);
+    let randomNumber = Math.floor(Math.random() * 1000);
+    let dateTime = new Date().toLocaleString();
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
     cell1.innerText = randomNumber;
     cell2.innerText = dateTime;
     rowCount++;
@@ -22,7 +22,7 @@ const addDataRow = () => {
 
 // Start generating new rows of data at the specified interval
 const startGenerating = () => {
-    const interval = parseInt(intervalInput.value) * 1000;
+    let interval = parseInt(intervalInput.value) * 1000;
     timerId = setInterval(addDataRow, interval);
     startBtn.disabled = true;
     stopBtn.disabled = false;
